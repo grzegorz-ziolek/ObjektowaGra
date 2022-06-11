407efebe1d946a702ef9b8de57de980550072eca
grzegorz.ziolek <104593451+grzegorz-ziolek@users.noreply.github.com>
2022-06-11 09:56:51 +0200

    Update Program.cs

--- ObjektowaGra/ObjektowaGra/Program.cs
+++ ObjektowaGra/ObjektowaGra/Program.cs
@@ -1,16 +1,19 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System;
 using System.IO;
 
 namespace ObjektowaGra
 {
 
+    //lista enum na trudnosc, obecnie nigdzie nie uzywana, moze do wywalenia
     enum difficulties
     {
         easy,
         medium,
         hard
     }
+    
+    //klasa item, używana do przetrzymywania pytań
     public class Item
     {
         public string category;
@@ -20,8 +23,47 @@ namespace ObjektowaGra
         public string correct_answer;
         public string[] incorrect_asnwers = new string[0];
     }
+    
+    //klasa gracza, zarówno AI jak i nasz
+    public class Player
+    {
+        //zmienne
+        int health;
+        int wrongCount;
+        int rightCount;
+	
+        //konstruktor gracza
+        public Player(int desiredHealth)
+        {
+            health = desiredHealth;
+            wrongCount = 0;
+            rightCount = 0;
+        }
+	
+        //metoda zadania obrażeń
+        public static void damageTaken(Player hero, List<Item> lista)
+        {
+            switch (lista.difficulty)
+            {
+                case "easy":
+                    hero.health = hero.health - 1;
+                    break;
+                case "medium":
+                    hero.health = hero.health - 2;
+                    break;
+                case "hard":
+                    hero.health = hero.health - 4;
+                    break;
+                default:
+                    Console.WriteLine("Błąd obliczania obrażeń");
+                    break;
+            }
+        }
+    }
+    
     internal class Program: Item
     {
+        //import pytań w formacie JSON
         public static List<Item> LoadJson()
         {
             using (StreamReader r = new StreamReader("questions.json"))
@@ -31,6 +73,8 @@ namespace ObjektowaGra
                 return items;
             }
         }
+        
+        //MAIN METHOD
         static void Main(string[] args)
         {
             Console.WriteLine(LoadJson());
