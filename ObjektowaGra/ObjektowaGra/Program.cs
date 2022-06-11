439798bdcc98923c106425e065dc5d5cf08982f2
grzegorz.ziolek <104593451+grzegorz-ziolek@users.noreply.github.com>
2022-06-11 07:39:50 +0200

    Add files via upload
    Core of the program
    Basic, currently not fully functional, import of json question database

---
+++ ObjektowaGra/ObjektowaGra/Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace ObjektowaGra
+{
+
+    enum difficulties
+    {
+        easy,
+        medium,
+        hard
+    }
+    public class Item
+    {
+        public string category;
+        public string type;
+        public string difficulty;
+        public string question;
+        public string correct_answer;
+        public string[] incorrect_asnwers = new string[0];
+    }
+    internal class Program: Item
+    {
+        public static List<Item> LoadJson()
+        {
+            using (StreamReader r = new StreamReader("questions.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+                return items;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LoadJson());
+            Console.ReadKey();
+        }
+    }
+}
